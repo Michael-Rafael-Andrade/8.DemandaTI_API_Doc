0986acef5310f864200c0276ae9cf7ef1613f263
Michael-Rafael-Andrade <michaelrafaeldeandrade@gmail.com>
2026-06-01 22:31:02 -0300

    Configuração do swagger na rota e controller
    Configuração do swagger na rota e controller

--- routes/rotasDemandas.js
+++ routes/rotasDemandas.js
@@ -3,18 +3,113 @@ var router = express.Router();
 var controllerDemandas = require('../controller/controllerDemandas.js');
 
 // Listar / filtrar
-router.get('/', controllerDemandas.listar);
+router.get('/',
+    /*
+    #swagger.tags    = ['Demandas']
+    #swagger.summary = 'Lista todas as demandas'
+    #swagger.parameters['status'] = {
+        in: 'query',
+        type: 'string',
+        enum: [
+            'pendente',
+            'em_andamento',
+            'concluido'
+            ],
+        description: 'Filtra pelo status' 
+        }
+        #swagger.response[200] = {
+            schema: {
+                type: 'array',
+                items: {
+                    $ref: '#/definitions/Demanda'                  
+                      
+                }
+            }
+        }
+    */
+    controllerDemandas.listar
+);
 
 // Obter por id
-router.get('/:id', controllerDemandas.obter);
+router.get('/:id',
+    /*
+        #swagger.tags       = ['Demandas']
+        #swagger.summary    = 'Retorna uma demanda pelo ID'
+        #swagger.parameters['id'] = {
+            in: 'path',
+            required: true,
+            type: 'integer',
+            description: 'ID da demanda'
+        }
+        #swagger.response[200] = {
+            schema: {
+                $ref: '#/definitions/Demanda'
+            }
+        }
+    */
+    controllerDemandas.obter
+);
 
 // Criar
-router.post('/', controllerDemandas.criar);
+router.post('/',
+    /*
+        #swagger.tags       = ['Demandas']
+        #swagger.summary    = 'Cria uma nova demanda'
+        #swagger.parameters['body'] = {
+            in: 'body',
+            required: true,
+            schema: {
+                $ref: '#/definitions/NovaDemanda'
+            }
+        }
+        #swagger.response[201] = {
+            $ref: '#/definitions/Demanda'
+        }
+    }
+    
+    */
+    controllerDemandas.criar
+);
+
 
 // Atualizar (parcial ou total)
-router.put('/:id', controllerDemandas.atualizar);
+router.put('/:id',
+    /*
+        #swagger.tags       = ['Demandas']
+        #swagger.summary    = 'Atualiza uma demanda existente'
+        #swagger.parameters['id'] = {
+            in: 'path',
+            required: true,
+            type: 'integer',
+            description: 'ID da demanda'
+        }
+        
+        #swagger.response[200] = {
+            schema: {
+                $ref: '#/definitions/Demanda'
+            }
+        }
+    
+    */
+
+    controllerDemandas.atualizar
+
+);
 
 // Deletar
-router.delete('/:id', controllerDemandas.deletar);
+router.delete('/:id',
+    /*
+        #swagger.tags       = ['Demandas']
+        #swagger.summary    = 'Remove uma demanda'
+        #swagger.parameters['id'] = {
+            in: 'path',
+            required: true,
+            type: 'integer',
+            description: 'ID da demanda'
+        }
+    */
+
+    controllerDemandas.deletar
+);
 
 module.exports = router;
--- routes/rotasIndex.js
+++ routes/rotasIndex.js
@@ -2,10 +2,16 @@ var express = require('express');
 var router = express.Router();
 const controllerIndex = require('../controller/controllerIndex.js');
 
+
+
 /* GET home page */
-router.get('/', controllerIndex.index);
+router.get('/', /* swagger.tags = ['Index']
+    #swagger.summary = 'Retorna informações gerais da API' */ controllerIndex.index
+);
 
 /* Página serviços */
-router.get('/servicos', controllerIndex.lista_servicos);
+router.get('/servicos', /* #swagger.tags    = ['Index']
+    #swagger.summary = Lista os serviços disponíveis na API' */ controllerIndex.lista_servicos
+);
 
 module.exports = router;
