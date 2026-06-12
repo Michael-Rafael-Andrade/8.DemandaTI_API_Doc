abfa2fbea134ad655aa664f43ec14d2a82f11bb4
Michael-Rafael-Andrade <michaelrafaeldeandrade@gmail.com>
2026-06-11 22:44:36 -0300

    Update rotasUsuarios.js

--- routes/rotasUsuarios.js
+++ routes/rotasUsuarios.js
@@ -3,9 +3,26 @@ var router = express.Router();
 var controllerUsuarios = require('../controller/controllerUsuarios');
 
 // Rota pública: cadastro de novo usuário
-router.post('/cadastro', controllerUsuarios.cadastro);
+router.post('/cadastro', 
+    /*
+    #swagger.tags = ['Usuarios']
+    #swagger.summary = 'Cadastra um novo usuário'
+    #swagger.parametes['body'] = { in: 'body', required: true, schema: { $ref: '#/definitions/NovoUsuario'}}
+    #swagger.responses[201] = { schema: { $ref: '#/definitions/UsuarioCriado' }}
+    #swagger.responses[400] = { description: 'Dados inválidos ou e-mail já cadastrado' }
+    */
+    controllerUsuarios.cadastro);
 
 // Rota pública: login - retorna token JWT em caso de sucesso
-router.post('/login', controllerUsuarios.login);
+router.post('/login', 
+    /*
+    #swagger.tags = ['Usuarios']
+    #swagger.summary = 'Autentica o usuário e retorna um token JWT'
+    #swagger.parameters['body'] = { in: 'body', required: true, schema: { $ref: '#/definitions/LoginUsuario' }}
+    #swagger.responses[401] = { description: 'Credenciais inválidas' }
+    */
+
+    controllerUsuarios.login
+);
 
 module.exports = router;
